1c6315414ce731aef18123e3ee0dfc023044420d
SAURAV_PANTH <panthsaurav17@gmail.com>
2026-02-15 16:19:05 +0530

    V1 with display, number, String is complete 15:17 15-02-2026

---
+++ MonoRun.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class MonoRun {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+        Integer a = 10;
+        String b = "fdsfd";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
